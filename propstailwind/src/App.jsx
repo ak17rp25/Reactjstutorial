a8944c22ff9af6ab6dcf7eb75fe33ce654550a92
ak17rp25 <ak17rp25@gmail.com>
2024-11-22 21:00:53 +0530

    passwordGEnerator

--- propstailwind/src/App.jsx
+++ propstailwind/src/App.jsx
@@ -1,9 +1,13 @@
-import Cards from './Cards'
+import { useState } from "react";
 function App() {
+  const [color, setcolor] = useState('green');
   return (
-    <div>
-      <Cards/>
-      <Cards/>
+    <div className="w-full h-screen" style={{backgroundColor:color}} >
+      <div className="fixed flex flex-wrap inset-2 justify-center bottom-12">
+        <button onClick={() => setcolor('red')}>Red</button>
+        <button onClick={() => setcolor('blue')}>Blue</button>
+        <button onClick={() => setcolor('green')}>Green</button>
+      </div>
     </div>
    
   );
---
+++ propstailwind/src/ColorChooser.jsx
@@ -0,0 +1,7 @@
+export default function ColorChooser(){
+    return(
+       <div>
+        
+       </div>
+    );
+}
